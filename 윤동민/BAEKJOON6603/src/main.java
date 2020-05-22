c0747ddfa08e359dd395a615c9ee0962dab3b525
dongminyoon <ehdals8965@naver.com>
2020-05-22 21:04:48 +0900

    update: 입력받는 로직 추가

--- 윤동민/BAEKJOON6603/src/main.java
+++ 윤동민/BAEKJOON6603/src/main.java
@@ -5,6 +5,23 @@ import java.io.InputStreamReader;
 public class main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+
+        String[] input;
+        int k;
+        int[] sample;
+
+        do {
+            input = br.readLine().split(" ");
+            k = Integer.parseInt(input[0]);
+            if (k == 0) break;
+            sample = new int[k];
+            for(int i = 1; i <=k; i++) { sample[i-1] = Integer.parseInt(input[i]); }
+
+            for(int i = 0; i < k; i++) { System.out.println(sample[i] + " "); }
+        } while (true);
+    }
+
+    static void dfs(int n) {
+
     }
 }
