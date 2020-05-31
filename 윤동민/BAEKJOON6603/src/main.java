8eaa9e187b953cad56424d8ca7a4c6be7c83024f
dongminyoon <ehdals8965@naver.com>
2020-05-31 13:58:54 +0900

    upload: 프렌즈4블록 구현

--- 윤동민/BAEKJOON6603/src/main.java
+++ 윤동민/BAEKJOON6603/src/main.java
@@ -1,30 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class main {
+    static int[] sample;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String[] input;
         int k;
-        int[] sample;
 
         do {
             input = br.readLine().split(" ");
             k = Integer.parseInt(input[0]);
             if (k == 0) break;
-            sample = new int[k];
+            main.sample = new int[k];
+
             for(int i = 1; i <=k; i++) { sample[i-1] = Integer.parseInt(input[i]); }
 
-            for(int i = 0; i < k; i++) { System.out.println(sample[i] + " "); }
+            for(int i = 0; i < k; i++) {
+                ArrayList<Integer> lotto = new ArrayList<>();
+                lotto.add(sample[i]);
+                dfs(lotto, i+1);
+            }
+            System.out.println("");
         } while (true);
-
-        System.out.println("aa");
     }
 
 
-    static void dfs(int n) {
+    static void dfs(ArrayList<Integer> lotto, int index) {
+        if (lotto.size() == 6) {
+            for(int i = 0; i  < 6; i++) { System.out.print(lotto.get(i) + " "); }
+            System.out.println("");
+            return;
+        }
 
+        for(int i = index; i < sample.length; i++) {
+            lotto.add(sample[i]);
+            dfs(lotto, i+1);
+            lotto.remove(lotto.size()-1);
+        }
     }
 }
