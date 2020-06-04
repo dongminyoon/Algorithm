5490d833717835cbbc3ccd4af1922f489a262a75
dongminyoon <ehdals8965@naver.com>
2020-06-04 15:55:58 +0900

    upload: 백준 1463번 다이나믹 프로그래밍 문제 풀이

---
+++ 윤동민/BAEKJOON1464/src/main.java
@@ -0,0 +1,54 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class main {
+    static int[] count = new int[1000001];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        calculate(n);
+        System.out.println(count[1]);
+
+    }
+
+    static public void calculate(int n) {
+        if(n == 1) { return; }
+        int number;
+        if(n % 3 == 0) {
+            number = n/3;
+            if(count[number] != 0 && count[number] > count[n] + 1) {
+                count[number] = count[n] + 1;
+                calculate(number);
+            } else if(count[number] == 0) {
+                count[number] = count[n] + 1;
+                calculate(number);
+            }
+        }
+
+        if (n % 2 == 0) {
+            number = n/2;
+            if(count[number] != 0 && count[number] > count[n] + 1) {
+                count[number] = count[n] + 1;
+                calculate(number);
+            } else if(count[number] == 0) {
+                count[number] = count[n] + 1;
+                calculate(number);
+            }
+        }
+
+        if (n - 1 >= 0) {
+            number = n-1;
+            if(count[number] != 0 && count[number] > count[n] + 1) {
+                count[number] = count[n] + 1;
+                calculate(number);
+            } else if (count[number] == 0) {
+                count[number] = count[n] + 1;
+                calculate(number);
+            }
+        }
+    }
+}
