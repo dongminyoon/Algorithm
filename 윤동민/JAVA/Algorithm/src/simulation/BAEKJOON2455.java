97a9c361112ad9bc3c142e193ae5a6a7e86804d3
dongminyoon <ehdals8965@naver.com>
2020-06-18 23:52:43 +0900

    upload: 2455번 지능형 기차 풀이

---
+++ 윤동민/JAVA/Algorithm/src/simulation/BAEKJOON2455.java
@@ -0,0 +1,11 @@
+package simulation;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BAEKJOON2455 {
+    public static void main(String[] args) throws IOException {
+        
+    }
+}
