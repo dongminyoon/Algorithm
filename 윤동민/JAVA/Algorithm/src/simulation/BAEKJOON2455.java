02f7e6248ae16c2ffd7d6e9ff676dd825582b6f6
dongminyoon <ehdals8965@naver.com>
2020-06-19 00:00:55 +0900

    upload: 2455번 지능형 기차 풀이 완성

--- 윤동민/JAVA/Algorithm/src/simulation/BAEKJOON2455.java
+++ 윤동민/JAVA/Algorithm/src/simulation/BAEKJOON2455.java
@@ -6,6 +6,20 @@ import java.io.InputStreamReader;
 
 public class BAEKJOON2455 {
     public static void main(String[] args) throws IOException {
-        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input;
+
+        int total_people = 0;
+        int max_people = 0;
+
+        for(int i = 0; i < 4; i++) {
+            input = br.readLine().split(" ");
+            total_people -= Integer.parseInt(input[0]);
+            total_people += Integer.parseInt(input[1]);
+            if(total_people > max_people) { max_people = total_people; }
+        }
+
+        System.out.println(max_people);
     }
 }
