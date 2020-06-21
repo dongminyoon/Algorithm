9000c1335c95ffd25ed5a5cd446997af5c215e32
dongminyoon <ehdals8965@naver.com>
2020-06-22 04:04:45 +0900

    upload: 백준 11726 타일 문제 풀이

--- 윤동민/JAVA/Algorithm/src/DynamicProgramming/BAEKJOON11726.java
+++ 윤동민/JAVA/Algorithm/src/DynamicProgramming/BAEKJOON11726.java
@@ -4,10 +4,29 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
+
+// DP 문제 처음 접해봄
 public class BAEKJOON11726 {
+    static int n;
+    static int[] d;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
+        d = new int[1001];
+        d[1] = 1;
+        d[2] = 2;
+
+        System.out.println(dfs(n));
+    }
+
+    static int dfs(int length) {
+        if(length == 1) { return 1; }
+        if(length == 2) { return 2; }
+        if(d[length] > 0) return d[length];
+
+        d[length] = (dfs(length-1) + dfs(length-2)) % 10007;
+        return d[length];
     }
 }
