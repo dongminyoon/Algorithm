568adf4f1a9263c322d571de7689032596771994
dongminyoon <ehdals8965@naver.com>
2020-06-21 23:55:18 +0900

    upload: 백준 11726번 2*n 타일링

---
+++ 윤동민/JAVA/Algorithm/src/DynamicProgramming/BAEKJOON11726.java
@@ -0,0 +1,13 @@
+package DynamicProgramming;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class BAEKJOON11726 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+    }
+}
