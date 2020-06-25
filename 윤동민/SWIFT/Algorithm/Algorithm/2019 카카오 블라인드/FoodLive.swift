9c24408c6a278373b76dd2701e6bc91d468787f1
dongminyoon <ehdals8965@naver.com>
2020-06-25 21:18:04 +0900

    upload: 카카오 2019 블라인드 무지 먹방 라이브 풀이

---
+++ 윤동민/SWIFT/Algorithm/Algorithm/2019 카카오 블라인드/FoodLive.swift
@@ -0,0 +1,15 @@
+//
+//  FoodLive.swift
+//  Algorithm
+//
+//  Created by 윤동민 on 2020/06/25.
+//  Copyright © 2020 윤동민. All rights reserved.
+//
+
+import Foundation
+
+struct FoodLive {
+    static func solution(_ food_times:[Int], _ k:Int64) -> Int {
+        return 0
+    }
+}
