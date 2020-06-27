110ff20bc0df49080565aa48fb25ac0cfe1ca2c8
dongminyoon <ehdals8965@naver.com>
2020-06-27 20:47:00 +0900

    upload: 백준 2579 중간까지 풀이

--- 윤동민/JAVA/Algorithm/src/DynamicProgramming/BAEKJOON2579.java
+++ 윤동민/JAVA/Algorithm/src/DynamicProgramming/BAEKJOON2579.java
@@ -1,9 +1,43 @@
 package DynamicProgramming;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class BAEKJOON2579 {
+    static int n;
+    static int[] points;
+    static int[] total;
+
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        points = new int[n+1];
+
+        for(int i = 1; i < n+1; i++) { points[i] = Integer.parseInt(br.readLine()); }
+
+    }
+
+    static void dfs(int currentStair, int continueCount) {
+        if (currentStair == 0) { return; }
+
+        if (currentStair-1)
+
+        // 2개의 칸을 이미 연속으로 밟은 경우
+        if (continueCount == 2 && currentStair-2 < 0) {
+            total[currentStair-2] = total[currentStair] + points[currentStair-2];
+            dfs(currentStair-2, 0);
+        } else if (points[currentStair-1] > points[currentStair-2]) {
+
+        }
+
+
+
+
+
+        continueCount += 1;
+
 
     }
 }
