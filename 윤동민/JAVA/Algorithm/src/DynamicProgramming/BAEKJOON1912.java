af3cf39b57317a5d8113c1594d095132276eb4db
dongminyoon <ehdals8965@naver.com>
2020-07-03 21:06:46 +0900

    upload: 백준 1912번 문제

--- 윤동민/JAVA/Algorithm/src/DynamicProgramming/BAEKJOON1912.java
+++ 윤동민/JAVA/Algorithm/src/DynamicProgramming/BAEKJOON1912.java
@@ -4,10 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class BAEKJOON2156 {
+public class BAEKJOON1912 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-
     }
 }
