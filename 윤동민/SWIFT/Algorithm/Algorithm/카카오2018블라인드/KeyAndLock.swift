23da38250dd3ebe1844e7c10750cc8e9c44e0521
dongminyoon <ehdals8965@naver.com>
2020-07-11 21:55:21 +0900

    upload: 열쇠와 자물쇠 문제

---
+++ 윤동민/SWIFT/Algorithm/Algorithm/카카오2018블라인드/KeyAndLock.swift
@@ -0,0 +1,15 @@
+//
+//  KeyAndLock.swift
+//  Algorithm
+//
+//  Created by 윤동민 on 2020/07/11.
+//  Copyright © 2020 윤동민. All rights reserved.
+//
+
+import Foundation
+
+struct KeyAndLock {
+    static func solution(_ key:[[Int]], _ lock:[[Int]]) -> Bool {
+        return true
+    }
+}
