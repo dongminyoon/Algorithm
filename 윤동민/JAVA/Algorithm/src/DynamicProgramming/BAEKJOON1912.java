1c579b289c6f2dc89cdfb30879d7bf750548fef7
dongminyoon <ehdals8965@naver.com>
2020-09-30 20:34:09 +0900

    feat: 백준 1912번 연구소 문제 풀이

--- 윤동민/JAVA/Algorithm/src/DynamicProgramming/BAEKJOON1912.java
+++ 윤동민/JAVA/Algorithm/src/DynamicProgramming/BAEKJOON1912.java
@@ -5,9 +5,32 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class BAEKJOON1912 {
+    static int N;
+    static int[] numbers;
+    static int max = -1001;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        br.readLine();
+        N = Integer.parseInt(br.readLine());
+
+        String[] input = br.readLine().split(" ");
+        numbers = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(input[i]);
+        }
+
+        int[] dp = new int[N];
+        dp[0] = numbers[0];
+        max = numbers[0];
+
+        for(int i = 1; i < N; i++) {
+            dp[i] = Math.max(dp[i-1] + numbers[i], numbers[i]);
+            max = Math.max(dp[i], max);
+        }
+
+
+        System.out.println(max);
     }
 }
