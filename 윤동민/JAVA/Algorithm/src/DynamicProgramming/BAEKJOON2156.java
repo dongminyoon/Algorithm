a6d12aede323a868878bf62360f7528cb17a59a5
dongminyoon <ehdals8965@naver.com>
2020-10-01 23:54:18 +0900

    feat: 백준 2156 포도주 문제 풀이

--- 윤동민/JAVA/Algorithm/src/DynamicProgramming/BAEKJOON2156.java
+++ 윤동민/JAVA/Algorithm/src/DynamicProgramming/BAEKJOON2156.java
@@ -8,6 +8,38 @@ public class BAEKJOON2156 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int N = Integer.parseInt(br.readLine());
 
+        int[] grapeAmount = new int[N];
+
+        for(int i = 0; i < N; i++) {
+            grapeAmount[i] = Integer.parseInt(br.readLine());
+        }
+
+        if (N == 1) {
+            System.out.println(grapeAmount[0]);
+            return;
+        } else if (N == 2) {
+            System.out.println(grapeAmount[0] + grapeAmount[1]);
+            return;
+        } else if (N == 3) {
+            int max = Math.max(grapeAmount[0] + grapeAmount[1], grapeAmount[0] + grapeAmount[2]);
+            max = Math.max(max, grapeAmount[1] + grapeAmount[2]);
+            System.out.println(max);
+            return;
+        }
+
+        int[] dp = new int[N];
+        dp[0] = grapeAmount[0];
+        dp[1] = grapeAmount[0] + grapeAmount[1];
+        int max = Math.max(grapeAmount[0] + grapeAmount[1], grapeAmount[0] + grapeAmount[2]);
+        max = Math.max(max, grapeAmount[1] + grapeAmount[2]);
+        dp[2] = max;
+
+        for(int i = 3; i < N; i++) {
+            dp[i] = Math.max(dp[i-2] + grapeAmount[i], grapeAmount[i-1] + dp[i-3] + grapeAmount[i]);
+        }
+
+        System.out.println(Math.max(dp[N-1], dp[N-2]));
     }
 }
