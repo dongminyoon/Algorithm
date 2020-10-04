d3c2a13fbadcd58639e411a706b7ee50a96aa301
dongminyoon <ehdals8965@naver.com>
2020-10-04 22:49:46 +0900

    feat: 백준 2217번 로프 풀이

--- 윤동민/JAVA/Algorithm/src/Greedy/BAEKJOON2217.java
+++ 윤동민/JAVA/Algorithm/src/Greedy/BAEKJOON2217.java
@@ -1,4 +1,29 @@
 package Greedy;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class BAEKJOON2217 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] weights = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            weights[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(weights);
+        int max = 0;
+
+        for (int i = 0; i < N; i++) {
+            int totalWeight = weights[i] * (N-i);
+            if (totalWeight > max) { max = totalWeight; }
+        }
+
+        System.out.println(max);
+    }
 }
