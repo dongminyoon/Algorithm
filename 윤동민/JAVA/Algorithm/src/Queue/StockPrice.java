014cced3026d381c92d566c8bfa788e25613ba62
dongminyoon <ehdals8965@naver.com>
2020-10-11 23:46:32 +0900

    feat: 프로그래머스 주식 문제 풀이

---
+++ 윤동민/JAVA/Algorithm/src/Queue/StockPrice.java
@@ -0,0 +1,36 @@
+package Queue;
+
+import java.util.LinkedList;
+
+public class StockPrice {
+    public static void main(String[] args) {
+        int[] prices = {1, 2, 3, 2, 3, 3, 1};
+
+        solution(prices);
+    }
+
+    public static int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+
+        LinkedList<Integer> stack = new LinkedList<>();
+
+        for (int i = 0; i < prices.length; i++) {
+            int curPrice = prices[i];
+
+            for (int j = 0; j < stack.size(); j++) {
+                answer[stack.get(j)] += 1;
+                if (curPrice < prices[stack.get(j)]) {
+                    stack.remove(j);
+                }
+            }
+
+            stack.add(i);
+        }
+
+        for (int i = 0; i < answer.length; i++) {
+            System.out.print(answer[i] + " ");
+        }
+
+        return answer;
+    }
+}
