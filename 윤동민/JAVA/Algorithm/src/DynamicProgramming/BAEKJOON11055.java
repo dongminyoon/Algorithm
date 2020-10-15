09fb1b82699f4911bf7a6f9d30d7bb35e3a51a94
dongminyoon <ehdals8965@naver.com>
2020-10-15 23:45:42 +0900

    feat: 백준 11055번 문제 풀이

--- 윤동민/JAVA/Algorithm/src/DynamicProgramming/BAEKJOON11055.java
+++ 윤동민/JAVA/Algorithm/src/DynamicProgramming/BAEKJOON11055.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class BAEKJOON11053 {
+public class BAEKJOON11055 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -17,22 +17,24 @@ public class BAEKJOON11053 {
             sequence[i] = Integer.parseInt(input[i]);
 
         int[] DP = new int[N];
+        int[] DP2 = new int[N];
+        DP[0] = 1;
+        DP2[0] = sequence[0];
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i < N; i++) {
             DP[i] = 1;
-        }
-
-        for (int i = 0; i < N; i++) {
+            DP2[i] = sequence[i];
             for (int j = 0; j < i; j++) {
                 if (sequence[i] > sequence[j] && DP[i] <= DP[j]) {
                     DP[i] = DP[j] + 1;
+                    DP2[i] = Math.max(DP2[j] + sequence[i], DP2[i]);
                 }
             }
         }
 
-        int max = 0;
+        int max = DP2[0];
         for (int i = 0; i < N; i++) {
-            if (max < DP[i]) { max = DP[i]; }
+            max = Math.max(max, DP2[i]);
         }
 
         System.out.println(max);
