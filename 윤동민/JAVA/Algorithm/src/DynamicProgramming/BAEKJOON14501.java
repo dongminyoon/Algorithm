1588ebd174848e3a1c9ec5c6fb4a2c8260950e1d
dongminyoon <ehdals8965@naver.com>
2020-11-09 03:55:13 +0900

    feat: 백준 14501번 - 퇴사 풀이 완료

--- 윤동민/JAVA/Algorithm/src/DynamicProgramming/BAEKJOON14501.java
+++ 윤동민/JAVA/Algorithm/src/DynamicProgramming/BAEKJOON14501.java
@@ -9,6 +9,33 @@ public class BAEKJOON14501 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        
+        int N = Integer.parseInt(br.readLine());
+
+        int[] T = new int[N+1];
+        int[] P = new int[N+1];
+
+        // i번째 날까지 일해서 받은 돈 최
+        int[] DP = new int[N+2];
+
+        for (int i = 1; i <= N; i++) {
+            String[] input = br.readLine().split(" ");
+            T[i] = Integer.parseInt(input[0]);
+            P[i] = Integer.parseInt(input[1]);
+        }
+
+        int max = DP[0];
+
+        for (int i = 1; i <= N; i++) {
+            max = Math.max(max, DP[i]);
+            DP[i] = max;
+
+            if (i + T[i] <= N+1) {
+                DP[i+T[i]] = Math.max(DP[i] + P[i], DP[i+T[i]]);
+            }
+        }
+
+
+        max = max > DP[N+1] ? max : DP[N+1];
+        System.out.println(max);
     }
 }
