ccd68ccdac8a7f97c4444ebb2a31c008468201c7
dongminyoon <ehdals8965@naver.com>
2021-02-04 01:55:20 +0900

    feat: 큰 수 만들기 풀이 - 시간초과 (String 배열 접근 문제 같음)

--- 윤동민/SWIFT/Algorithm/Algorithm/프로그래머스/MakeMaxNumber.swift
+++ 윤동민/SWIFT/Algorithm/Algorithm/프로그래머스/MakeMaxNumber.swift
@@ -9,7 +9,31 @@
 import Foundation
 
 struct MakeMaxNumber {
-    func solution() {
+    func solution(_ number:String, _ k:Int) -> String {
+        var answer: String = ""
         
+        let restNumber = number.count - k
+        
+        var startIndex: Int = 0
+        
+        for indexI in 0..<restNumber {
+            let startStringIndex = number.index(number.startIndex, offsetBy: startIndex)
+            var max: Int = Int(String(number[startStringIndex]))!
+                              
+            for indexJ in startIndex...number.count-(restNumber-indexI) {
+                let stringIndex = number.index(number.startIndex, offsetBy: indexJ)
+                let castingNumber = Int(String(number[stringIndex]))!
+                
+                if castingNumber > max {
+                    max = castingNumber
+                    startIndex = indexJ
+                }
+            }
+            
+            startIndex = startIndex + 1
+            answer += "\(max)"
+        }
+        
+        return answer
     }
 }
