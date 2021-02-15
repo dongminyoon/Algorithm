c558804ea2b5fbdaefc5a0720c69bc94d74842a3
dongminyoon <ehdals8965@naver.com>
2021-02-16 00:15:24 +0900

    feat: 프로그래머스 H-Index 풀이

--- 윤동민/SWIFT/Algorithm/Algorithm/프로그래머스/H-Index.swift
+++ 윤동민/SWIFT/Algorithm/Algorithm/프로그래머스/H-Index.swift
@@ -12,18 +12,21 @@ struct H_Index {
     func solution(_ citations:[Int]) -> Int {
         var answer: Int = 0
         
-        for indexI in 0..<citations.count {
-            var moreCount: Int = 0
-            var lessCount: Int = 0
-            for indexJ in 0..<citations.count {
-                if indexI == indexJ { continue }
-                
-                if citations[indexI] >= citations[indexJ] { moreCount += 1 }
-                if citations[indexI] <= citations[indexJ] { lessCount += 1 }
+        var h_index: Int = 0
+        
+        while h_index <= citations.count {
+            var moreThanHCount: Int = 0
+            
+            for citation in citations {
+                if h_index > citation { continue }
+                moreThanHCount += 1
             }
             
-            if moreCount != lessCount { continue }
-            answer = answer < moreCount ? moreCount : answer
+            if h_index <= moreThanHCount {
+                answer = h_index >= answer ? h_index : answer
+            }
+            
+            h_index += 1
         }
         
         return answer
