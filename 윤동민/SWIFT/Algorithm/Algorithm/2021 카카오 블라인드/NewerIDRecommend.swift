9cd9330bf67507c2dfbff51aaf32870b90512adc
dongminyoon <ehdals8965@naver.com>
2021-03-06 17:32:07 +0900

    feat: 2021 카카오 블라인드 - 신규 아이디 추천

---
+++ 윤동민/SWIFT/Algorithm/Algorithm/2021 카카오 블라인드/NewerIDRecommend.swift
@@ -0,0 +1,13 @@
+//
+//  NewerIDRecommend.swift
+//  Algorithm
+//
+//  Created by USER on 2021/03/06.
+//  Copyright © 2021 윤동민. All rights reserved.
+//
+
+import Foundation
+
+struct NewerIDReccomend {
+    
+}
