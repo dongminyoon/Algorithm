406b00f2aab7cfbd5da083b6bc1b08bf8885ff7f
dongminyoon <ehdals8965@naver.com>
2021-04-03 22:23:29 +0900

    feat: 백준 2110번 입력

--- 윤동민/SWIFT/Algorithm/Algorithm/BAEKJOON/BAEKJOON2110.swift
+++ 윤동민/SWIFT/Algorithm/Algorithm/BAEKJOON/BAEKJOON2110.swift
@@ -10,6 +10,15 @@ import Foundation
 
 struct BAEKJOON2110 {
     func solution() {
+        let input   = readLine()!.split(separator: " ").compactMap({ Int(String($0))! })
         
+        let N       = input[0]
+        let C       = input[1]
+        
+        var points: [Int] = []
+        for _ in 0..<N {
+            
+            points.append(<#T##newElement: Int##Int#>)
+        }
     }
 }
